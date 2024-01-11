fb1ceee0023fb190b0f7dbd5211f5049f2c2fefb
Bruno <bruno.kus@o2.pl>
2024-01-11 16:24:22 +0100

    już we właściwym folderze

--- src/main/java/com/example/itewriter/HelloApplication.java
+++ src/main/java/com/example/itewriter/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.itewriter;
 
+import com.example.itewriter.area.root.WidgetRoot;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,7 +12,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(new WidgetRoot(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
